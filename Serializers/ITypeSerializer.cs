6d1270199062127ee15e8cc6a68268150669aa34
Thad <wiredwiz@users.noreply.github.com>
2015-01-13 10:32:54 -0500

    Simplified ITypeSerializer.

--- Serializers/ITypeSerializer.cs
+++ Serializers/ITypeSerializer.cs
@@ -10,13 +10,7 @@ namespace Org.Edgerunner.DotSerialize.Serializers
 {
    public interface ITypeSerializer<T>
    {
-      T Deserialize(string xml);
-      T Deserialize(XmlDocument document);
-      T Deserialize(Stream stream);
-      T Deserialize(TextReader reader);
       T Deserialize(XmlReader reader);
-      void Serialize(Stream stream, T obj);
-      void Serialize(TextWriter writer, T obj);
       void Serialize(XmlWriter writer, T obj);
    }
 }
