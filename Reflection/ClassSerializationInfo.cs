d6cbddff03bca010efc18c3247c1c68287504fb3
Thaddeus Ryker <thad@edgerunner.org>
2015-01-12 22:20:08 -0500

    Added FasterFlect library to drastically reduce work and increase performance. Added serialization info classes.

---
+++ Reflection/ClassSerializationInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Edgerunner.DotSerialize.Reflection
+{
+   public class ClassSerializationInfo
+   {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ClassSerializationInfo"/> class.
+      /// </summary>
+      /// <param name="className"></param>
+      /// <param name="fullyQualifiedTypeName"></param>
+      public ClassSerializationInfo(string className, string fullyQualifiedTypeName)
+      {
+         ClassName = className;
+         EntityName = className;
+         FullyQualifiedTypeName = fullyQualifiedTypeName;
+         FieldInfoByName = new Dictionary<string, FieldSerializationInfo>();
+         FieldInfoByEntityName = new Dictionary<string, FieldSerializationInfo>();
+      }
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ClassSerializationInfo"/> class.
+      /// </summary>
+      /// <param name="className"></param>
+      /// <param name="fullyQualifiedTypeName"></param>
+      public ClassSerializationInfo(string className, string fullyQualifiedTypeName, IList<FieldSerializationInfo> fields)
+      {
+         ClassName = className;
+         EntityName = className;
+         FullyQualifiedTypeName = fullyQualifiedTypeName;
+         FieldInfoByName = new Dictionary<string, FieldSerializationInfo>();
+         FieldInfoByEntityName = new Dictionary<string, FieldSerializationInfo>();
+         foreach (FieldSerializationInfo field in fields)
+         {
+            FieldInfoByName.Add(field.FieldName, field);
+            FieldInfoByEntityName.Add(field.EntityName, field);
+         }
+      }
+
+      public string ClassName { get; set; }
+      public string FullyQualifiedTypeName { get; set; }
+      public string EntityName { get; set; }
+      public IDictionary<string, FieldSerializationInfo> FieldInfoByName { get; set; }
+      public IDictionary<string, FieldSerializationInfo> FieldInfoByEntityName { get; set; }
+   }
+}
---
+++ Reflection/FieldSerializationInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Edgerunner.DotSerialize.Reflection
+{
+   public class FieldSerializationInfo
+   {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="FieldSerializationInfo"/> class.
+      /// </summary>
+      /// <param name="fieldName"></param>
+      /// <param name="fullyQualifiedTypeName"></param>
+      public FieldSerializationInfo(string fieldName, string fullyQualifiedTypeName)
+      {
+         FieldName = fieldName;
+         FullyQualifiedTypeName = fullyQualifiedTypeName;
+         EntityName = fieldName;
+         IsElement = true;
+      }
+      /// <summary>
+      /// Initializes a new instance of the <see cref="FieldSerializationInfo"/> class.
+      /// </summary>
+      /// <param name="fieldName"></param>
+      /// <param name="entityName"></param>
+      /// <param name="fullyQualifiedTypeName"></param>
+      public FieldSerializationInfo(string fieldName, string entityName, string fullyQualifiedTypeName)
+      {
+         FieldName = fieldName;
+         EntityName = entityName;
+         FullyQualifiedTypeName = fullyQualifiedTypeName;
+         IsElement = true;
+      }
+      /// <summary>
+      /// Initializes a new instance of the <see cref="FieldSerializationInfo"/> class.
+      /// </summary>
+      /// <param name="fieldName"></param>
+      /// <param name="entityName"></param>
+      /// <param name="fullyQualifiedTypeName"></param>
+      /// <param name="isElement"></param>
+      public FieldSerializationInfo(string fieldName, string entityName, string fullyQualifiedTypeName, bool isElement)
+      {
+         FieldName = fieldName;
+         EntityName = entityName;
+         FullyQualifiedTypeName = fullyQualifiedTypeName;
+         IsElement = isElement;
+      }
+      public string FieldName { get; set; }
+      public string EntityName { get; set; }
+      public string FullyQualifiedTypeName { get; set; }
+      public bool IsElement { get; set; }
+   }
+}
