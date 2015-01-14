cc2b396706a36467ed48e99e3d9cf7d52246ae0e
Thad <wiredwiz@users.noreply.github.com>
2015-01-14 16:28:01 -0500

    Refactored initial cache to an Interface and created two implementations. Added cache calls to TypeInspector. Removed unsupported targets from Xml attribute classes.

---
+++ Reflection/Caching/SerializationInfoCacheBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Edgerunner.DotSerialize.Reflection.Caching
+{
+   public abstract class SerializationInfoCacheBase<T> : ISerializationInfoCache
+   {
+      protected readonly Dictionary<Type, T> _InternalCache;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="SerializationInfoCache"/> class.
+      /// </summary>
+      protected SerializationInfoCacheBase()
+      {
+         _InternalCache = new Dictionary<Type, T>();
+      }
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="SerializationInfoCache"/> class.
+      /// </summary>
+      /// <param name="capacity"></param>
+      protected SerializationInfoCacheBase(int capacity)
+      {
+         _InternalCache = new Dictionary<Type, T>(capacity);
+      }
+
+      public abstract void AddInfo(TypeSerializationInfo info);
+      public abstract TypeSerializationInfo GetInfo(Type type);
+   }
+}
