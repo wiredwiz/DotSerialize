5d92edda9fdcf8716f71cb5e844e83b2c91bf875
Thad <wiredwiz@users.noreply.github.com>
2015-01-16 17:14:23 -0500

    Added TypeSerializerFactory and tweaked code.

---
+++ Serializers/TypeSerializerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace Org.Edgerunner.DotSerialize.Serializers
+{
+   public class TypeSerializerFactory : ITypeSerializerFactory
+   {
+      protected IKernel Kernel { get; set; }
+      /// <summary>
+      /// Initializes a new instance of the <see cref="TypeSerializerFactory"/> class.
+      /// </summary>
+      /// <param name="kernel"></param>
+      public TypeSerializerFactory(IKernel kernel)
+      {
+         Kernel = kernel;
+      }
+
+      public ITypeSerializer<T> GetTypeSerializer<T>()
+      {
+         return Kernel.Get<ITypeSerializer<T>>();
+      }
+   }
+}
