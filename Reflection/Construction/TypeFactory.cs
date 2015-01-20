a61d98d39ce51e4dd8db051ee8e9f3edc8609901
Thad <wiredwiz@users.noreply.github.com>
2015-01-20 03:00:48 -0500

    Initial working version of new type creator.

--- Reflection/Construction/TypeFactory.cs
+++ Reflection/Construction/TypeFactory.cs
@@ -19,13 +19,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Fasterflect;
 
 namespace Org.Edgerunner.DotSerialize.Reflection.Construction
 {
    public static class TypeFactory
    {
+      private static TypeConstructorCache Cache { get; set; }
+
+      static TypeFactory()
+      {
+         Cache = new TypeConstructorCache();
+      }
+
       public static T CreateInstance<T>(IDictionary<TypeMemberSerializationInfo, object> data)
       {
          return (T)CreateInstance(typeof(T), data);
@@ -33,27 +39,82 @@ namespace Org.Edgerunner.DotSerialize.Reflection.Construction
 
       public static object CreateInstance(Type type, IDictionary<TypeMemberSerializationInfo, object> data)
       {
-         object result = type.TryCreateInstance((from x in data.Keys
-                                                 select x.ConstructorFriendlyName).ToArray<string>(),
-                                                (from x in data.Keys
-                                                 select x.DataType).ToArray<Type>(),
-                                                data.Values.ToArray()
-            );
+         //object result = type.TryCreateInstance((from x in data.Keys
+         //                                        select x.ConstructorFriendlyName).ToArray<string>(),
+         //                                       (from x in data.Keys
+         //                                        select x.DataType).ToArray<Type>(),
+         //                                       data.Values.ToArray()
+         //   );
+         object result = null;
 
-         List<ConstructorInfo> constructors = type.Constructors() as List<ConstructorInfo>;
-         constructors.Sort(delegate(ConstructorInfo x, ConstructorInfo y)
+         object[] paramValues;
+         var memberInfoList = data.Keys.ToList();
+         var cachedMap = Cache.GetMappingFor(type, memberInfoList);
+         if (cachedMap != null)
          {
-            return x.Parameters().Count.CompareTo(y);
-         });
-         //if (constructors[0].Parameters().Count == 0)
-         //   result = constructors[0].
+            paramValues = BuildParameterVaules(cachedMap.Parameters, data);
+            result = cachedMap.Constructor.Invoke(paramValues);
+         }
+         else
+         {
+            var constructors = type.Constructors().OrderBy(x => x.Parameters().Count).ToList();
+            foreach (var constructor in constructors)
+               try
+               {
+                  Dictionary<string, TypeMemberSerializationInfo> paramMap;
+                  if (constructor.Parameters().Count == 0)
+                  {
+                     paramMap = new Dictionary<string, TypeMemberSerializationInfo>();
+                     result = constructor.Invoke(null);
+                  }
+                  else
+                  {                     
+                     paramMap = ParameterMapper.MapTypeMembersToParameters(type, constructor.Parameters(), memberInfoList);
+                     paramValues = BuildParameterVaules(paramMap, data);
+                     result = constructor.Invoke(paramValues);
+                  }
+
+                  if (result != null)
+                     Cache.AddMappingFor(type, memberInfoList, new ConstructorMap(constructor, paramMap));
+                     break;
+               }
+               catch (Exception ex)
+               {
+               }
+         }
+         if (result == null)
+            throw new Exception(string.Format("Unable to create instance of type \"{0}\"", type.Name()));
+
+         foreach (var memberInfo in data.Keys)
+         {
+            switch (memberInfo.Type)
+            {
+               case TypeMemberSerializationInfo.MemberType.Field:
+                  result.SetFieldValue(memberInfo.Name, data[memberInfo]);
+                  break;
+               case TypeMemberSerializationInfo.MemberType.Property:
+                  result.SetPropertyValue(memberInfo.Name, data[memberInfo]);
+                  break;
+            }
+         }
 
          return result;
       }
 
-      public static TypeMemberSerializationInfo FindMatchingMember()
+      private static object[] BuildParameterVaules(Dictionary<string, TypeMemberSerializationInfo> parameterMap,
+                                                 IDictionary<TypeMemberSerializationInfo, object> data)
       {
-         
+         var paramValues = new object[parameterMap.Count];
+         int counter = 0;
+         foreach (var pair in parameterMap)
+         {
+            if (pair.Value == null)
+               paramValues[counter] = null;
+            else
+               paramValues[counter] = data[pair.Value];
+            counter++;
+         }
+         return paramValues;
       }
    }
 }
