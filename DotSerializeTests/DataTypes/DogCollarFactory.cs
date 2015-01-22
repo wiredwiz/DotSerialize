bcca5589e0043544ae1dc9f89d165672a7f7ee48
Thad <wiredwiz@users.noreply.github.com>
2015-01-21 19:05:08 -0500

    Closer to working array deserialization.

---
+++ DotSerializeTests/DataTypes/DogCollarFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotSerializeTests.DataTypes
+{
+   public static class DogCollarFactory
+   {
+      public static DogCollar GetCollar(int length, bool studded)
+      {
+         return new DogCollar(length, studded);
+      }
+   }
+}
