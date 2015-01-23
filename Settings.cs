734e073490e00260cecd02b748f17e387a4ac9d0
Thaddeus Ryker <thad@edgerunner.org>
2015-01-22 22:54:05 -0500

    Added settings class.

---
+++ Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Edgerunner.DotSerialize
+{
+   public class Settings
+   {
+      public List<Attribute> AttributesToIgnore { get; set; }
+      public CultureInfo Culture { get; set; }
+
+   }
+}
