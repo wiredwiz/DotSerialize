a0c3482657e26a9920efff00138a2263a5fd99ce
Thaddeus Ryker <thad@edgerunner.org>
2015-02-24 21:19:59 -0500

    Added support for helper factories.

--- Reflection/Construction/TypeFactory.cs
+++ Reflection/Construction/TypeFactory.cs
@@ -22,16 +22,18 @@ using System.Linq;
 using System.Reflection;
 using Fasterflect;
 using Org.Edgerunner.DotSerialize.Utilities;
-
+using System.Data;
 namespace Org.Edgerunner.DotSerialize.Reflection.Construction
 {
    public static class TypeFactory
    {
       private static TypeConstructorCache Cache { get; set; }
+      private static Dictionary<Type, IHelperFactory> Helpers { get; set; }
 
       static TypeFactory()
       {
          Cache = new TypeConstructorCache();
+         Helpers = new Dictionary<Type, IHelperFactory>();
       }
 
       #region Static Methods
@@ -106,13 +108,30 @@ namespace Org.Edgerunner.DotSerialize.Reflection.Construction
          return paramValues;
       }
 
+      /// <summary>Attempts to create an instance of T and populate its members with data from the supplied IDictionary.</summary>
+      /// <typeparam name="T">Type of instance to be created.</typeparam>
+      /// <param name="data">IDictionary containing mappings of <see cref="Org.Edgerunner.DotSerialize.Reflection.TypeMemberInfo"/> instances to object data.</param>
+      /// <returns>A new instance of type T populated with the data from the data argument.</returns>
+      /// <exception caption="" cref="System.TypeLoadException">T is not a valid type.</exception>
+      /// <exception caption="" cref="System.Reflection.TargetInvocationException">Unable to automatically create a new instance of type.</exception>
+      /// <seealso cref="Org.Edgerunner.DotSerialize.Reflection.TypeMemberInfo"/>
       public static T CreateInstance<T>(IDictionary<TypeMemberInfo, object> data)
       {
          return (T)CreateInstance(typeof(T), data);
       }
 
+      /// <summary>Attempts to create an instance of Type and populate its members with data from the supplied IDictionary.</summary>
+      /// <param name="data">IDictionary containing mappings of <see cref="Org.Edgerunner.DotSerialize.Reflection.TypeMemberInfo"/> instances to object data.</param>
+      /// <param name="type">Type of instance to be created.</param>
+      /// <returns>A new instance of type Type populated with the data from the data argument.</returns>
+      /// <exception caption="" cref="System.ArgumentNullException">type is null</exception>
+      /// <exception caption="" cref="System.Reflection.TargetInvocationException">Unable to automatically create a new instance of type.</exception>
+      /// <seealso cref="Org.Edgerunner.DotSerialize.Reflection.TypeMemberInfo"/>
       public static object CreateInstance(Type type, IDictionary<TypeMemberInfo, object> data)
       {
+         if (Helpers.ContainsKey(type))
+            return Helpers[type].CreateInstance();
+
          object result = null;
 
          var memberInfoList = data.Keys.ToList();
@@ -152,6 +171,25 @@ namespace Org.Edgerunner.DotSerialize.Reflection.Construction
             }
          result = result.UnwrapIfWrapped();
          return result;
+      }
+
+      /// <summary>
+      /// Registers an implementation of IHelperFactory to be used when creating new instances of Type T.
+      /// </summary>
+      /// <param name="helper">Instance of IHelperFactory.</param>
+      /// <typeparam name="T">Type that the IHelperFactory will create.</typeparam>
+      public static void RegisterHelper<T>(IHelperFactory helper)
+      {
+         Helpers[typeof(T)] = helper;
+      }
+
+      /// <summary>
+      /// Unregisters an implementation of IHelperFactory for Type T.
+      /// </summary>
+      /// <typeparam name="T">Type to unregister IHelperFactory from.</typeparam>
+      public static void UnregisterHelper<T>()
+      {
+         Helpers.Remove(typeof(T));
       }
 
       #endregion
