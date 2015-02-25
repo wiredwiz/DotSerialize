8fbd5bab3341f8b4d15853ef406693f8b1fe38eb
Thaddeus Ryker <thad@edgerunner.org>
2015-02-24 23:36:27 -0500

    Moved TypeInspectorTests to a subfolder for consistency.

--- DotSerializeTests/TypeInspectorTests/TypeInspectorTests.cs
+++ DotSerializeTests/TypeInspectorTests/TypeInspectorTests.cs
@@ -9,19 +9,19 @@ namespace Org.Edgerunner.DotSerialize.Tests
    public class TypeInspectorTests
    {
       [TestMethod]
-      public void InspectDogReturnsInfoWithFiveTypeMembers()
+      public void InspectDogReturnsInfoWithNineTypeMembers()
       {
          var info = new Org.Edgerunner.DotSerialize.Reflection.TypeInspector().GetInfo(typeof(Dog));
-         Assert.AreEqual<int>(5, info.MemberInfoByEntityName.Count);
-         Assert.AreEqual<int>(5, info.MemberInfoByName.Count);
+         Assert.AreEqual<int>(9, info.MemberInfoByEntityName.Count);
+         Assert.AreEqual<int>(9, info.MemberInfoByName.Count);
       }
 
       [TestMethod]
-      public void InspectOwnerReturnsInfoWithThreeTypeMembers()
+      public void InspectOwnerReturnsInfoWithSixTypeMembers()
       {
          var info = new Org.Edgerunner.DotSerialize.Reflection.TypeInspector().GetInfo(typeof(Owner));
-         Assert.AreEqual<int>(3, info.MemberInfoByEntityName.Count);
-         Assert.AreEqual<int>(3, info.MemberInfoByName.Count);
+         Assert.AreEqual<int>(6, info.MemberInfoByEntityName.Count);
+         Assert.AreEqual<int>(6, info.MemberInfoByName.Count);
       }
 
       [TestMethod]
@@ -32,11 +32,11 @@ namespace Org.Edgerunner.DotSerialize.Tests
       }
 
       [TestMethod]
-      public void InspectPersonReturnsInfoWithOneTypeMember()
+      public void InspectPersonReturnsInfoWithFiveTypeMember()
       {
          var info = new Org.Edgerunner.DotSerialize.Reflection.TypeInspector().GetInfo(typeof(Person));
-         Assert.AreEqual<int>(1, info.MemberInfoByEntityName.Count);
-         Assert.AreEqual<int>(1, info.MemberInfoByName.Count);
+         Assert.AreEqual<int>(5, info.MemberInfoByEntityName.Count);
+         Assert.AreEqual<int>(5, info.MemberInfoByName.Count);
       }
 
       [TestMethod]
