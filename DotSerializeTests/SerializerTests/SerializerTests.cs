e659e7389542cd0b7bde4223e05787d8693f5863
Thaddeus Ryker <thad@edgerunner.org>
2015-02-24 22:28:56 -0500

    Fixed test problems.

---
+++ DotSerializeTests/SerializerTests/SerializerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ApprovalTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Org.Edgerunner.DotSerialize.Tests.DataTypes;
+
+namespace Org.Edgerunner.DotSerialize.Tests.SerializerTests
+{
+   /// <summary>
+   /// Summary description for SerializerTests
+   /// </summary>
+   [TestClass]
+   public class SerializerTests
+   {
+      private const string SerializeDogResultsInProperFile_Approved = "SerializerTests.SerializeDogResultsInProperFile.approved.xml";
+
+      [TestInitialize]
+      private void Setup()
+      {
+         Utilities.ExtractEmbeddedFile(SerializeDogResultsInProperFile_Approved);
+      }
+
+      [TestCleanup]
+      private void CleanUp()
+      {
+         Utilities.DeleteFile(SerializeDogResultsInProperFile_Approved);
+      }
+
+      [TestMethod]
+      public void SerializeDogResultsInProperFile()
+      {
+         var dog = new Dog("Fido", "Golden Retriever", true, null);
+         var serializer = new Serializer();
+         string xml = serializer.SerializeObject(dog);
+         Approvals.VerifyXml(xml);
+      }
+   }
+}
