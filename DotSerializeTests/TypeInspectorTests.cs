f27d1499ec74e0c95ba73e14eb7570bece50aca0
Thad <wiredwiz@users.noreply.github.com>
2015-02-25 13:14:33 -0500

    Updated broken unit tests.

--- DotSerializeTests/TypeInspectorTests.cs
+++ DotSerializeTests/TypeInspectorTests.cs
@@ -12,31 +12,31 @@ namespace Org.Edgerunner.DotSerialize.Tests
       public void InspectDogReturnsInfoWithFiveTypeMembers()
       {
          var info = new Reflection.TypeInspector().GetInfo(typeof(Dog));
-         Assert.AreEqual<int>(5, info.MemberInfoByEntityName.Count);
-         Assert.AreEqual<int>(5, info.MemberInfoByName.Count);
+         Assert.AreEqual<int>(8, info.MemberInfoByEntityName.Count);
+         Assert.AreEqual<int>(8, info.MemberInfoByName.Count);
       }
 
       [TestMethod]
       public void InspectOwnerReturnsInfoWithThreeTypeMembers()
       {
          var info = new Reflection.TypeInspector().GetInfo(typeof(Owner));
-         Assert.AreEqual<int>(3, info.MemberInfoByEntityName.Count);
-         Assert.AreEqual<int>(3, info.MemberInfoByName.Count);
+         Assert.AreEqual<int>(6, info.MemberInfoByEntityName.Count);
+         Assert.AreEqual<int>(6, info.MemberInfoByName.Count);
       }
 
       [TestMethod]
       public void InspectOwnerReturnsInfoWithRootNodeNameDogOwner()
       {
          var info = new Reflection.TypeInspector().GetInfo(typeof(Owner));
-         Assert.AreEqual<string>("DogOwner", info.EntityName);
+         Assert.AreEqual<string>("PetOwner", info.EntityName);
       }
 
       [TestMethod]
       public void InspectPersonReturnsInfoWithOneTypeMember()
       {
          var info = new Reflection.TypeInspector().GetInfo(typeof(Person));
-         Assert.AreEqual<int>(1, info.MemberInfoByEntityName.Count);
-         Assert.AreEqual<int>(1, info.MemberInfoByName.Count);
+         Assert.AreEqual<int>(5, info.MemberInfoByEntityName.Count);
+         Assert.AreEqual<int>(5, info.MemberInfoByName.Count);
       }
 
       [TestMethod]
