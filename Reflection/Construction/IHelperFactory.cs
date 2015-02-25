f70eb895ac59aac812a53e5ed8252a7294bcb260
Thad <wiredwiz@users.noreply.github.com>
2015-02-24 21:19:59 -0500

    Added support for helper factories.

---
+++ Reflection/Construction/IHelperFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Org.Edgerunner.DotSerialize.Reflection.Construction
+{
+   /// <summary>Defines a method to create an instance of some type.</summary>
+   public interface IHelperFactory
+   {
+      /// <summary>Creates a new instance of some type.</summary>
+      /// <returns>A new instance of some Type.</returns>
+      object CreateInstance();
+   }
+}
