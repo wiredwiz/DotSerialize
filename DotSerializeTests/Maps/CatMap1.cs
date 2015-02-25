12b7f680e8f8fc591cf12351c1c7f5d2257b978d
Thad <wiredwiz@users.noreply.github.com>
2015-02-24 23:00:53 -0500

    Added more unit tests.

--- DotSerializeTests/Maps/CatMap1.cs
+++ DotSerializeTests/Maps/CatMap1.cs
@@ -1,24 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Org.Edgerunner.DotSerialize.Mapping;
 using Org.Edgerunner.DotSerialize.Tests.DataTypes;
 
 namespace Org.Edgerunner.DotSerialize.Tests.Maps
 {
-   public sealed class CatMap : XmlClassMap<Cat>
+   public sealed class CatMap1 : XmlClassMap<Cat>
    {
       /// <summary>
-      /// Initializes a new instance of the <see cref="CatMap"/> class.
+      /// Initializes a new instance of the <see cref="CatMap1"/> class.
       /// </summary>
-      public CatMap()
+      public CatMap1()
       {
          Map(x => x.Name).OrderedAs(0);
          Map(x => x.Breed).OrderedAs(2);
          Map(x => x.Age).OrderedAs(1);
-         Map(x => x.Selfish).UsingName("IsACat").AsAttribute();
+         Map(x => x.Selfish).UsingName("ActsLikeACat").AsAttribute();
       }
    }
 }
--- DotSerializeTests/SerializerTests/SerializerTests.cs
+++ DotSerializeTests/SerializerTests/SerializerTests.cs
@@ -1,28 +1,65 @@
-﻿using System;
+﻿#region Apache License 2.0
+
+// Copyright 2015 Thaddeus Ryker
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
 using ApprovalTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Org.Edgerunner.DotSerialize.Tests.DataTypes;
+using Org.Edgerunner.DotSerialize.Tests.Maps;
+
+// ReSharper disable InconsistentNaming
 
 namespace Org.Edgerunner.DotSerialize.Tests.SerializerTests
 {
    /// <summary>
-   /// Summary description for SerializerTests
+   ///    Summary description for SerializerTests
    /// </summary>
    [TestClass]
    public class SerializerTests
    {
-      private const string SerializeDogResultsInProperFile_Approved = "SerializerTests.SerializeDogResultsInProperFile.approved.xml";
+      private const string SerializeDogResultsInProperFile_Approved =
+         "SerializerTests.SerializeDogResultsInProperFile.approved.xml";
+
+      private const string SerializeCatWithoutMapResultsInProperOutput_Approved =
+         "SerializerTests.SerializeCatWithoutMapResultsInProperOutput.approved.xml";
+
+      private const string SerializeCatWithMap1ResultsInProperOutput_Approved =
+         "SerializerTests.SerializeCatWithMap1ResultsInProperOutput.approved.xml";
+
+      private const string SerializeCatWithMap2ResultsInProperOutput_Approved =
+         "SerializerTests.SerializeCatWithMap2ResultsInProperOutput.approved.xml";
 
       [TestInitialize]
       private void Setup()
       {
          Utilities.ExtractEmbeddedFile(SerializeDogResultsInProperFile_Approved);
+         Utilities.ExtractEmbeddedFile(SerializeCatWithoutMapResultsInProperOutput_Approved);
+         Utilities.ExtractEmbeddedFile(SerializeCatWithMap1ResultsInProperOutput_Approved);
+         Utilities.ExtractEmbeddedFile(SerializeCatWithMap2ResultsInProperOutput_Approved);
       }
 
       [TestCleanup]
       private void CleanUp()
       {
          Utilities.DeleteFile(SerializeDogResultsInProperFile_Approved);
+         Utilities.DeleteFile(SerializeCatWithoutMapResultsInProperOutput_Approved);
+         Utilities.DeleteFile(SerializeCatWithMap1ResultsInProperOutput_Approved);
+         Utilities.DeleteFile(SerializeCatWithMap2ResultsInProperOutput_Approved);
       }
 
       [TestMethod]
@@ -31,6 +68,35 @@ namespace Org.Edgerunner.DotSerialize.Tests.SerializerTests
          var dog = new Dog("Fido", "Golden Retriever", true, null);
          var serializer = new Serializer();
          string xml = serializer.SerializeObject(dog);
+         Approvals.VerifyXml(xml);
+      }
+
+      [TestMethod]
+      public void SerializeCatWithoutMapResultsInProperOutput()
+      {
+         var cat = new Cat { Name = "Puss", Breed = "Saimese", Selfish = true, Age = 3 };
+         var serializer = new Serializer();
+         string xml = serializer.SerializeObject(cat);
+         Approvals.VerifyXml(xml);
+      }
+
+      [TestMethod]
+      public void SerializeCatWithMap1ResultsInProperOutput()
+      {
+         var cat = new Cat { Name = "Puss", Breed = "Saimese", Selfish = true, Age = 3 };
+         var serializer = new Serializer();
+         serializer.RegisterClassMap<CatMap1>();
+         string xml = serializer.SerializeObject(cat);
+         Approvals.VerifyXml(xml);
+      }
+
+      [TestMethod]
+      public void SerializeCatWithMap2ResultsInProperOutput()
+      {
+         var cat = new Cat { Name = "Puss", Breed = "Saimese", Selfish = true, Age = 3 };
+         var serializer = new Serializer();
+         serializer.RegisterClassMap<CatMap2>();
+         string xml = serializer.SerializeObject(cat);
          Approvals.VerifyXml(xml);
       }
    }
