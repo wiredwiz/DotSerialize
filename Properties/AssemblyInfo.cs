2f1625357cb3c83b84f77e31b587b358ad27fdf8
Thad <wiredwiz@users.noreply.github.com>
2015-02-27 19:55:38 -0500

    Made small change to unit tests and updated version to 1.2 release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -54,6 +54,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.13.58")]
-[assembly: AssemblyFileVersion("1.1.13.58")]
+[assembly: AssemblyVersion("1.2.1.59")]
+[assembly: AssemblyFileVersion("1.2.1.59")]
 [assembly: InternalsVisibleTo("DotSerializeTests")]
