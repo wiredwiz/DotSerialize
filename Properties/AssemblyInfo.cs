58c2e1736fbbf6ecf3781e1aded2faefbc75c1c9
Thaddeus Ryker <thad@edgerunner.org>
2015-12-28 03:07:57 -0500

    Finished support for multi-dimensional arrays along with debugging.  All tests now pass and these changes are ready for release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -54,6 +54,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.6.99")]
-[assembly: AssemblyFileVersion("1.2.6.99")]
+[assembly: AssemblyVersion("1.2.7.0")]
+[assembly: AssemblyFileVersion("1.2.7.0")]
 [assembly: InternalsVisibleTo("DotSerializeTests")]
